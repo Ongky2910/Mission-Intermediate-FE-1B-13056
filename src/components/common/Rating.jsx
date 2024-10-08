646946de7c3dd322b524e58d11777da49b4a3ab8
Ongky2910 <ongkypermana21@gmail.com>
2024-10-08 21:06:49 +0800

    Create Rating.jsx

---
+++ src/components/common/Rating.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Rating = ({ rating, showRating }) => {
+  const filledStars = Math.floor(rating); 
+  const halfStar = rating % 1 >= 0.5; 
+
+  return (
+    <div className="flex items-center text-white">
+    <span className="text-white font-serif">
+       {'★'.repeat(filledStars)} 
+      {'☆'.repeat(5 - filledStars - (halfStar ? 1 : 0))} 
+    </span>
+    {showRating && <span className="text-white ml-2">{rating}/5</span>}
+    </div>
+  );
+};
+
+export default Rating;
