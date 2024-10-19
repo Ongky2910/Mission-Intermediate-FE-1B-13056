afc44cb9cac938718ebdccea4adec45ad87199d0
Ongky2910 <ongkypermana21@gmail.com>
2024-10-19 16:20:20 +0800

    Update GenreDrop.jsx

--- src/components/common/GenreDrop.jsx
+++ src/components/common/GenreDrop.jsx
@@ -2,41 +2,40 @@ import React, { useState } from 'react';
 import clsx from 'clsx';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 
-const Dropdown = ({ options, className, onChange, isMobile}) => {
+const Dropdown = ({ options, className, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState("");
 
   const handleOptionClick = (value) => {
-    setSelectedOption(value);  
-    onChange(value);           
-    setIsOpen(false);          
+    setSelectedOption(value);
+    onChange(value);
+    setIsOpen(false);
   };
 
   return (
     <div className={clsx("relative", className)}>
       <button 
         className={clsx(
-          "flex items-center justify-between w-30 rounded-lg bg-gray-input text-white py-2 px-4 focus:outline-none",
-           isMobile ? "bg-gray-input text-sm" : "bg-gray-800"
-          ,className
+          "flex items-center justify-between w-full rounded-lg bg-gray-input text-white py-2 px-4 focus:outline-none",
+          className
         )}
         onClick={() => setIsOpen(!isOpen)}
       >
         <span>{selectedOption || "Genre"}</span>
-        {isMobile && <RiArrowDropDownLine className=" text-white" size={20} />} {/* Down arrow */}
+        <RiArrowDropDownLine className="text-white" size={20} />
       </button>
       {isOpen && (
         <div className={clsx(
-          "absolute z-10 w-full bg-gray-input rounded shadow-lg",
-          isMobile ? "w-30" : "w-50" 
+          "absolute z-30 bg-gray-input rounded shadow-lg max-h-[400px] overflow-y-auto",
+          "w-[100px] md:w-[400px]"  // Full width
         )}>
-          <div className="md:grid grid-cols-2 p-2 max-h-60 overflow-y-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 p-2">
             {options.map((option) => (
               <button
                 key={option.value}
                 className={clsx(
-                  "text-left p-1 w-full hover:bg-gray-700",
-                  selectedOption === option.value && "bg-gray-600" 
+                  "text-left p-2 hover:bg-gray-700 w-full",
+                  selectedOption === option.value && "bg-gray-600"
                 )}
                 onClick={() => handleOptionClick(option.value)}
               >
