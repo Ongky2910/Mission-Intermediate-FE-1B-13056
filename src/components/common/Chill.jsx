1f7cdde301c219773ad8ffad1321ba5da2c01de5
Ongky2910 <ongkypermana21@gmail.com>
2024-10-19 16:19:28 +0800

    Update Chill.jsx

--- src/components/common/Chill.jsx
+++ src/components/common/Chill.jsx
@@ -4,7 +4,8 @@ import clsx from 'clsx';
 const Logo = ({ src, alt, text, className }) => {
   return (
     <div className={clsx('flex justify-center items-center mb-5', className)}>
-      <img src={src} alt={alt} className="max-w-xs" />
+      <img src={src} alt={alt} className="max-w-7xl mt-2" />
+      <span className="text-center text-4xl hidden md:block mt-2 ml-1 uppercase font-londrina">{text}</span>
     </div>
   );
 };
